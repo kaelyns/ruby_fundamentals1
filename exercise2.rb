bdb76d0912d06c0f6f911497cada3fd254ddfeea
kaelynsuh <kaelyn.suh@gmail.com>
2016-01-12 14:59:48 -0500

    create exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# Exercise 2
+
+# q1
+puts "A 15% tip for a $55 meal is $#{55*0.15}"
+
+# q2
+puts "string" + 5.to_s
+
+# q3
+puts "45628".to_i * "7839".to_i
+
+# q4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
