3f9a210b10b1b541ef45149fbcd690204f6b7f45
kaelynsuh <kaelyn.suh@gmail.com>
2016-01-12 16:01:25 -0500

    create multiples or 3 and/or 5

---
+++ exercise4.rb
@@ -0,0 +1,13 @@
+# Exercise 4
+
+(1..100).each do |number|
+    if number % 15 == 0
+        puts "BitMaker"
+    elsif number % 5 == 0
+        puts "Maker"
+    elsif number % 3 == 0
+        puts "Bit"
+    else
+        puts number
+    end
+end
