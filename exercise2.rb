5e7698d7f3dc32232ee2c682391a748ee3fd4eaf
kaelynsuh <kaelyn.suh@gmail.com>
2016-01-12 15:24:00 -0500

    more changes to q3

--- exercise2.rb
+++ exercise2.rb
@@ -7,7 +7,7 @@ puts "A 15% tip for a $55 meal is $#{55*0.15}"
 puts "string" + 5.to_s
 
 # q3
-puts 45628.to_s.to_i * 7839.to_s.to_i
+puts "#{45628 * 7839}"
 
 # q4
 puts (10 < 20 && 30 < 20) || !(10 == 11)
